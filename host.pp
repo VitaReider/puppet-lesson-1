82a1369d145ee8f84758cd57d43163a4255774fd
lev-tmp <l.evepstein@gmail.com>
2017-12-29 22:17:08 +0100

    initial commit

---
+++ host.pp
@@ -0,0 +1,4 @@
+host { 'puppet-standalone-ubuntu':
+  ip           => '192.168.30.12',
+  host_aliases => 'ubuntu-puppet',
+}
---
+++ ssh.pp
@@ -0,0 +1,5 @@
+ssh_authorized_key { 'tux@centos7':
+  user => 'localhost.localdomain',
+  type => 'ssh-rsa',
+  key  => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDoDZJvfbESCtjHZXiuqhcqep52qHOcy4OI/lmQp7JM/hmXGPOqm8NECzQVImIlC3LDy9D6lAOZofgfahSpSlH8XYG1RoBH5PAluSgSRfVKSlJzWE1gWqdB2k9lXhYt4axLpflqIXu0e/9/u00Bpr5X2dMyy85ikf7PxeHcbvOMhe8Zg5LhCJMeSM+z6erUml857up5ivvcnqx4a9cUJoPmGUywNFPDSIeVcums99uZSzHrRb2abFtYpL/k/R3izQ0ZvI/BcwzhAxmutjN/jKWsAbxBREeSbskUoCm/mBPdqxZQj+blzQmtqfokUC2WRpADgGaCUWrkqDXpXSZPMC8T'
+}
